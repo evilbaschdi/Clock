74f3621fdc21203c6492d494376271cb4b7039da
Sebastian Walter <sebwalter@gmx.net>
2023-02-01 20:16:38 +0100

    M  Clock/AboutWindow.xaml.cs M  Clock/Clock.csproj M  global.json

--- Clock/AboutWindow.xaml.cs
+++ Clock/AboutWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
 using EvilBaschdi.About.Core.Models;
 using JetBrains.Annotations;
+using Wpf.Ui.Controls;
 
 namespace Clock;
 
@@ -8,7 +9,7 @@ namespace Clock;
 ///     Interaction logic for AboutWindow.xaml
 /// </summary>
 // ReSharper disable once RedundantExtendsListEntry
-public partial class AboutWindow
+public partial class AboutWindow : UiWindow
 {
     [NotNull] private readonly IAboutModel _aboutModel;
 
